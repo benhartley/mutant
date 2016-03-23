a01f5b96812379c29df96b3096fe5ca989d8231d
Benedict Hartley <spam@chaffin.ch>
2016-03-23 22:11:08 +0000

    Add placeholder tests

---
+++ test/lib/index.js
@@ -0,0 +1,15 @@
+import '../../register';
+
+import test from 'ava';
+
+test('example1', t => {
+    t.pass();
+});
+
+test('example2', t => {
+    t.pass();
+});
+
+// test('example3', t => {
+//     t.fail();
+// });
