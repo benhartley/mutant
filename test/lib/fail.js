dd10a55f36a85543496026851e84288561055dae
Benedict Hartley <spam@chaffin.ch>
2016-03-27 21:55:31 +0100

    Add tests for Fail lib

---
+++ test/lib/fail.js
@@ -0,0 +1,45 @@
+require('../../lib/register');
+var expect = require('chai').expect;
+var rewire = require('rewire');
+var sinon = require('sinon');
+var underTest = rewire('../../lib/fail');
+
+describe('fail lib', function() {
+
+    var restore;
+
+    beforeEach(function() {
+        restore = underTest.__set__({
+            chalk: {
+                red: sinon.spy()
+            },
+            console: {
+                error: sinon.spy()
+            },
+            figures: {
+                cross: sinon.stub().returns('cross')
+            },
+            process: {
+                exit: sinon.spy()
+            }
+        });
+        underTest('example failure');
+    });
+
+    it('should call console.error', function() {
+        expect(underTest.__get__('console').error.called);
+    });
+
+    it('should call chalk.red with constructed message', function() {
+        expect(underTest.__get__('chalk').red.calledWith('cross Error: example failure\n'));
+    });
+
+    it('should call figures.cross', function() {
+        expect(underTest.__get__('figures').cross.called);
+    });
+
+    afterEach(function() {
+        restore();
+    });
+
+});
