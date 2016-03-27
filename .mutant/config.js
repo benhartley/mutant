ebd0d8d382933e289c69e1932d6dfc9f7045c348
Benedict Hartley <spam@chaffin.ch>
2016-03-28 00:11:12 +0100

    Load mutation plugins from config file

--- .mutant/config.js
+++ .mutant/config.js
@@ -2,6 +2,8 @@ module.exports = {
     tests: {
         run: 'npm t -- --reporter tap',
         glob: './test/**/*.js'
-    }
-
+    },
+    mutations: [
+        'boolean-literal-flip'
+    ]
 };
