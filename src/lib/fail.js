2984b1cf4b94119f3d7fb61135190c170ff572fc
Benedict Hartley <spam@chaffin.ch>
2016-03-28 13:13:48 +0100

    Switch fail lib to ES6

---
+++ src/lib/fail.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+const figures = require('figures');
+
+module.exports = (message) => {
+    console.error(chalk.red(`${figures.cross} Error: ${message}\n`));
+    return process.exit(1);
+};
+
