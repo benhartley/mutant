d6e34a98748e7823d9be51719dd0628dee4a92b4
Benedict Hartley <spam@chaffin.ch>
2016-03-28 13:09:42 +0100

    Add ES6 mutator lib

---
+++ src/lib/mutator.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const babylon = require('babylon');
+const babelGenerator = require('babel-generator')['default'];
+const babelTraverse = require('babel-traverse')['default'];
+const config = require('./config');
+
+const mutations = config.get('mutations', [])
+    .reduce((acc, mutation) => {
+        acc[mutation] = require('../plugins/mutations/' + mutation);
+        return acc;
+    }, {});
+
+module.exports = (filename) => {
+    const code = fs.readFileSync(filename, 'utf8');
+    const ast = babylon.parse(code);
+    babelTraverse(ast, mutations['boolean-literal-flip']);
+    return babelGenerator(ast, null, code).code;
+};
+
