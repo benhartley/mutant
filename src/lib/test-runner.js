ed074f57d0a398087f42ac281bffb5393a706f95
Benedict Hartley <spam@chaffin.ch>
2016-03-28 13:29:08 +0100

    Switch test runner lib to ES6

---
+++ src/lib/test-runner.js
@@ -0,0 +1,20 @@
+const assign = require('lodash/assign');
+const bluebird = require('bluebird');
+const spawn = require('child_process').spawn;
+const tapParser = require('tap-parser');
+const config = require('./config');
+
+function processTestCommand() {
+    const [head, ...tail] = config.get('tests.run', '').split(' ');
+    return [head, tail];
+}
+
+module.exports = env => {
+    return new bluebird((resolve, reject) => {
+        const testProcess = spawn.apply(null, processTestCommand(), assign(process.env, env));
+        testProcess.stdout.pipe(tapParser(resolve));
+        testProcess.on('exit', code => {
+            if (code !== 0) {return reject();}
+        });
+    });
+};
