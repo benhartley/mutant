d5a36f0c9a945ddedd72a1e13d35b0fe972e0f60
Benedict Hartley <spam@chaffin.ch>
2016-03-31 00:37:45 +0100

    Hard-code env to pass into test runner

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
+const figures = require('figures');
+const get = require('lodash/get');
 const has = require('lodash/has');
 const testRunner = require('./test-runner');
 const fail = require('./fail');
@@ -12,8 +14,10 @@ function welcome() {
 `));
 }
 
-function handleInitialTestRun(result) {
-    if (result.ok && !has(result, 'failures')) {return;}
+function handleInitialTestRun(response) {
+    if (get(response, 'tap.ok') && !has(response, 'tap.failures')) {
+        return console.log(` ${chalk.green(figures.tick)} Initial test run OK!`);
+    }
     return fail('Initial test run failed - please check your tests are passing to begin.');
 }
 
@@ -21,6 +25,16 @@ function main() {
     welcome();
     return testRunner()
         .then(handleInitialTestRun)
+        .then(() => {
+            // get num cpus here and do map.concurrency
+            return testRunner({
+                MUTATION: 'boolean-literal-flip',
+                STATEMASK: '01'
+            });
+        })
+        .then(response => {
+            console.log(response);
+        })
         .catch(fail);
 }
 
