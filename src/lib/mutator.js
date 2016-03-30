e7faa129187dface93df1bbba52dfa48f91bd2f3
Benedict Hartley <spam@chaffin.ch>
2016-03-31 00:36:16 +0100

    Pass mutation into mutator lib via ENV

--- src/lib/mutator.js
+++ src/lib/mutator.js
@@ -2,18 +2,33 @@ const fs = require('fs');
 const babylon = require('babylon');
 const babelGenerator = require('babel-generator')['default'];
 const babelTraverse = require('babel-traverse')['default'];
+const get = require('lodash/get');
+const isFunction = require('lodash/isFunction');
 const config = require('./config');
+const fail = require('./fail');
 
 const mutations = config.get('mutations', [])
     .reduce((acc, mutation) => {
-        acc[mutation] = require('../plugins/mutations/' + mutation);
+        try {
+            acc[mutation] = require(`../plugins/mutations/${mutation}`);
+        } catch (err) {
+            fail(`Mutation plugin "${mutation}" does not exist. Please check your configuration.`);
+        }
         return acc;
     }, {});
+
+function getMutation() {
+    const mutation = get(mutations, process.env.MUTATION);
+    if (!isFunction(mutation)) {
+        return fail(`Plugin "${process.env.MUTATION}" does not return a function`);
+    }
+    return mutation(process.env.STATEMASK);
+}
 
 module.exports = filename => {
     const code = fs.readFileSync(filename, 'utf8');
     const ast = babylon.parse(code);
-    babelTraverse(ast, mutations['boolean-literal-flip']('01'));
+    babelTraverse(ast, getMutation());
     return babelGenerator(ast, null, code).code;
 };
 
