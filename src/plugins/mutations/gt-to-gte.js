a2f8040f6c8dbbffdb4f2fe02e7ec6be27305152
Benedict Hartley <spam@chaffin.ch>
2016-04-01 09:43:13 +0100

    Add GT to GTE mutation plugin

---
+++ src/plugins/mutations/gt-to-gte.js
@@ -0,0 +1,12 @@
+let n = 0;
+
+module.exports = stateMask => {
+    return {
+        BinaryExpression(path) {
+            if (stateMask.substr(n, 1) === '1' && path.node.operator === '>') {
+                path.node.operator = '>=';
+            }
+            n++;
+        }
+    };
+};
