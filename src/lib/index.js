c8327279fa99cfafe2ab8c73b0c2982715dcb8f8
Benedict Hartley <spam@chaffin.ch>
2016-04-01 23:43:59 +0100

    Update control flow in index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,9 @@
 require('source-map-support').install();
+const bluebird = require('bluebird');
 const commander = require('commander');
 const chalk = require('chalk');
 const figures = require('figures');
+const os = require('os');
 const get = require('lodash/get');
 const has = require('lodash/has');
 const testRunner = require('./test-runner');
@@ -18,6 +20,16 @@ function welcome() {
 `));
 }
 
+function runSingleMutation(testPath, previousState, previousResult) {
+    console.log('previousResult', previousResult);
+    return mutation => {
+        return testRunner(testPath, {
+            MUTATION: mutation,
+            STATEMASK: '01'
+        });
+    };
+}
+
 function handleInitialTestRun(response) {
     if (get(response, 'tap.ok') && !has(response, 'tap.failures')) {
         return console.log(` ${chalk.green(figures.tick)} Initial test run OK!`);
@@ -26,19 +38,15 @@ function handleInitialTestRun(response) {
 }
 
 function main(testPath) {
+    const concurrency = {concurrency: os.cpus().length};
     welcome();
     config.validate();
     return testRunner(testPath)
         .then(handleInitialTestRun)
         .then(() => {
-            // get num cpus here and do map.concurrency
-            return testRunner(testPath, {
-                MUTATION: 'boolean-literal-flip',
-                STATEMASK: '01'
-            });
-        })
-        .then(response => {
-            console.log(response);
+            return bluebird
+                .map(config.get('mutations'), runSingleMutation(testPath, ''), concurrency)
+                .map(console.log.bind(console, 'bluebird'));
         })
         .catch(fail);
 }
