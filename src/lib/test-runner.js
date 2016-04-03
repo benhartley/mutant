7b8ede139ef05ad9d123643e19d103fe87c3a11f
Benedict Hartley <spam@chaffin.ch>
2016-04-03 13:05:38 +0100

    Simplify test runner command construction

--- src/lib/test-runner.js
+++ src/lib/test-runner.js
@@ -3,19 +3,16 @@ const spawn = require('child_process').spawn;
 const tapParser = require('tap-parser');
 const config = require('./config');
 
-function processTestCommand(testPath) {
-    const command = config.get('tests.run').replace('$FILE', testPath);
-    const [head, ...tail] = command.split(' ');
-    return [head, tail];
+function getTestCommand(testPath) {
+    return config
+        .get('tests.run')
+        .replace('$FILE', testPath)
+        .split(' ');
 }
 
 module.exports = (params, callback) => {
-    const {testPath, env} = params;
-    const testProcess = spawn.apply(
-        null,
-        processTestCommand(testPath),
-        assign(process.env, env)
-    );
+    const [command, ...args] = getTestCommand(params.testPath);
+    const testProcess = spawn(command, args, assign(process.env, params.env));
     testProcess.stdout.pipe(tapParser(result => {
         return callback(null, assign(params, {tap: result}));
     }));
